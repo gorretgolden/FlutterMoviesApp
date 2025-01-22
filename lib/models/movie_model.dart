b0c53cb700f81d2bb66c571543c6633f506ff7d3
gorretgolden <nabatanzigorret143@gmail.com>
2025-01-22 18:35:01 +0300

    added movie model

---
+++ lib/models/movie_model.dart
@@ -0,0 +1,53 @@
+class Movie {
+  final bool adult;
+  final String backdropPath;
+  final List<int> genreIds;
+  final int id;
+  final String originalLanguage;
+  final String originalTitle;
+  final String overview;
+  final double popularity;
+  final String posterPath;
+  final String releaseDate;
+  final String title;
+  final bool video;
+
+  Movie({
+    required this.adult,
+    required this.backdropPath,
+    required this.genreIds,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.title,
+    required this.video,
+  });
+
+  // Factory method to create Movie object from JSON
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      adult: json['adult'],
+      backdropPath: json['backdrop_path'] ?? '',
+      genreIds: List<int>.from(json['genre_ids']),
+      id: json['id'] ?? 0,
+      originalLanguage: json['original_language'] ?? '',
+      originalTitle: json['original_title'] ?? '',
+      overview: json['overview'] ?? '',
+      popularity: json['popularity']?.toDouble() ?? 0.0,
+      posterPath: json['poster_path'] ?? '',
+      releaseDate: json['release_date'] ?? '',
+      title: json['title'] ?? '',
+      video: json['video'] ?? true,
+    );
+  }
+
+  // Method to get full URL for the poster image
+  String get fullPosterUrl => 'https://image.tmdb.org/t/p/w500$posterPath';
+
+  // Method to get full URL for the backdrop image
+  String get fullBackdropUrl => 'https://image.tmdb.org/t/p/w500$backdropPath';
+}
---
+++ lib/services/movie_service.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/movie_model.dart';
+import '../models/genre.dart';
+import '../models/actor.dart';
+
+class MovieService {
+  static const String _apiKey = 'YOUR_API_KEY';
+  static const String _baseUrl = 'https://api.themoviedb.org/3';
+
+  Future<List<Movie>> fetchPopularMovies() async {
+    final response = await http.get(Uri.parse('$_baseUrl/movie/popular?api_key=$_apiKey'));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return List<Movie>.from(data['results'].map((movie) => Movie.fromJson(movie)));
+    } else {
+      throw Exception('Failed to fetch popular movies');
+    }
+  }
+
+  Future<List<Genre>> fetchGenres() async {
+    final response = await http.get(Uri.parse('$_baseUrl/genre/movie/list?api_key=$_apiKey'));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return List<Genre>.from(data['genres'].map((genre) => Genre.fromJson(genre)));
+    } else {
+      throw Exception('Failed to fetch genres');
+    }
+  }
+
+  Future<List<Actor>> fetchPopularActors() async {
+    final response = await http.get(Uri.parse('$_baseUrl/person/popular?api_key=$_apiKey'));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return List<Actor>.from(data['results'].map((actor) => Actor.fromJson(actor)));
+    } else {
+      throw Exception('Failed to fetch actors');
+    }
+  }
+}
