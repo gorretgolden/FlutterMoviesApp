6475d0d76b4956a15fd624ee531c6f49740d25b7
gorretgolden <nabatanzigorret143@gmail.com>
2025-01-22 19:00:43 +0300

    updated movie player screen

--- lib/screens/movie_player_screen.dart
+++ lib/screens/movie_player_screen.dart
@@ -1,49 +1,49 @@
-import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
+// import 'package:flutter/material.dart';
+// import 'package:video_player/video_player.dart';
 
-class MoviePlayerScreen extends StatefulWidget {
-  final String moviePath; // The file path to the downloaded movie
+// class MoviePlayerScreen extends StatefulWidget {
+//   final String moviePath; // The file path to the downloaded movie
 
-  MoviePlayerScreen({required this.moviePath});
+//   MoviePlayerScreen({required this.moviePath});
 
-  @override
-  _MoviePlayerScreenState createState() => _MoviePlayerScreenState();
-}
+//   @override
+//   _MoviePlayerScreenState createState() => _MoviePlayerScreenState();
+// }
 
-class _MoviePlayerScreenState extends State<MoviePlayerScreen> {
-  late VideoPlayerController _controller;
+// class _MoviePlayerScreenState extends State<MoviePlayerScreen> {
+//   late VideoPlayerController _controller;
 
-  @override
-  void initState() {
-    super.initState();
-    _controller = VideoPlayerController.file(Uri.parse(widget.moviePath))
-      ..initialize().then((_) {
-        setState(() {});
-        _controller.play();
-      });
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller = VideoPlayerController.file(Uri.parse(widget.moviePath))
+//       ..initialize().then((_) {
+//         setState(() {});
+//         _controller.play();
+//       });
+//   }
 
-  @override
-  void dispose() {
-    super.dispose();
-    _controller.dispose();
-  }
+//   @override
+//   void dispose() {
+//     super.dispose();
+//     _controller.dispose();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Watching Movie'),
-        backgroundColor: Colors.deepPurple,
-      ),
-      body: Center(
-        child: _controller.value.isInitialized
-            ? AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
-              )
-            : CircularProgressIndicator(),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Watching Movie'),
+//         backgroundColor: Colors.deepPurple,
+//       ),
+//       body: Center(
+//         child: _controller.value.isInitialized
+//             ? AspectRatio(
+//                 aspectRatio: _controller.value.aspectRatio,
+//                 child: VideoPlayer(_controller),
+//               )
+//             : CircularProgressIndicator(),
+//       ),
+//     );
+//   }
+// }
