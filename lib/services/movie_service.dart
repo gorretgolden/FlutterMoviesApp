367f455465920b06210b5caa2ea219bf1a317672
gorretgolden <nabatanzigorret143@gmail.com>
2025-01-22 18:49:52 +0300

    added search screen

--- lib/services/movie_service.dart
+++ lib/services/movie_service.dart
@@ -37,4 +37,20 @@ class MovieService {
       throw Exception('Failed to fetch actors');
     }
   }
+
+  static Future<List<Movie>> searchMovies(String query) async {
+    final response = await http.get(
+      Uri.parse('$_baseUrl/search/movie?api_key=$_apiKey&query=$query'),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      List<Movie> movies = (data['results'] as List)
+          .map((movieData) => Movie.fromJson(movieData))
+          .toList();
+      return movies;
+    } else {
+      throw Exception('Failed to load movies');
+    }
+  }
 }
