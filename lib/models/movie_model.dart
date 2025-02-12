7c838a6d462eae5c272fdb192353014dd84d5ec0
gorretgolden <nabatanzigorret143@gmail.com>
2025-02-12 13:11:45 +0300

    updated model using quicktype

--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -1,75 +1,34 @@
-
 class Movie {
-  final String backdropPath;
-  final int id;
-  final String title;
-  final String originalTitle;
-  final String overview;
-  final String posterPath;
-  final String mediaType;
-  final bool adult;
-  final String originalLanguage;
-  final List<int> genreIds;
-  final double popularity;
-  final String releaseDate;
-  final bool video;
-  final double voteAverage;
-  final int voteCount;
+    bool adult;
+    String backdropPath;
+    List<int> genreIds;
+    int id;
+    String originalLanguage;
+    String originalTitle;
+    String overview;
+    double popularity;
+    String posterPath;
+    DateTime releaseDate;
+    String title;
+    bool video;
+    double voteAverage;
+    int voteCount;
 
-  Movie(
-      {required this.backdropPath,
-      required this.id,
-      required this.title,
-      required this.originalTitle,
-      required this.overview,
-      required this.posterPath,
-      required this.mediaType,
-      required this.adult,
-      required this.originalLanguage,
-      required this.genreIds,
-      required this.popularity,
-      required this.releaseDate,
-      required this.video,
-      required this.voteAverage,
-      required this.voteCount});
+    Movie({
+        required this.adult,
+        required this.backdropPath,
+        required this.genreIds,
+        required this.id,
+        required this.originalLanguage,
+        required this.originalTitle,
+        required this.overview,
+        required this.popularity,
+        required this.posterPath,
+        required this.releaseDate,
+        required this.title,
+        required this.video,
+        required this.voteAverage,
+        required this.voteCount,
+    });
 
-  factory Movie.fromJson(Map<String, dynamic> json) {
-    return Movie(
-      backdropPath: json['backdrop_path'],
-      id: json['id'],
-      title: json['title'],
-      originalTitle: json['original_title'],
-      overview: json['overview'],
-      posterPath: json['poster_path'],
-      mediaType: json['media_type'],
-      adult: json['adult'],
-      originalLanguage: json['original_language'],
-      genreIds: json['genre_ids'].cast<int>(),
-      popularity: json['popularity'],
-      releaseDate: json['release_date'],
-      video: json['video'],
-      voteAverage: json['vote_average'],
-      voteCount: json['vote_count'],
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['backdrop_path'] = backdropPath;
-    data['id'] = id;
-    data['title'] = title;
-    data['original_title'] = originalTitle;
-    data['overview'] = overview;
-    data['poster_path'] = posterPath;
-    data['media_type'] = mediaType;
-    data['adult'] = adult;
-    data['original_language'] = originalLanguage;
-    data['genre_ids'] = genreIds;
-    data['popularity'] = popularity;
-    data['release_date'] = releaseDate;
-    data['video'] = video;
-    data['vote_average'] = voteAverage;
-    data['vote_count'] = voteCount;
-    return data;
-  }
 }
